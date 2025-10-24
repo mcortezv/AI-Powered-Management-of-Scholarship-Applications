95a06aeee4f136139aa72b82742652f6013f0eea
mcortezv <mdjesuscv@gmail.com>
2025-10-23 18:51:33 -0700

    Historial Academico
    Co-authored-by: Eduardo Rodriguez <ewar795@gmail.com>

--- sistema_escolar/negocios_solicitarBeca/src/main/java/objetosNegocio/HistorialAcademico.java
+++ sistema_escolar/negocios_solicitarBeca/src/main/java/objetosNegocio/HistorialAcademico.java
@@ -4,11 +4,76 @@
  */
 
 package objetosNegocio;
+import objetosNegocio.enums.Carrera;
 
 /**
  *
  * @author Cortez, Manuel;
  */
 public class HistorialAcademico {
+    private Carrera carrera;
+    private double promedio;
+    private double procentajeBajas;
+    private double cargaAcademica;
+    private int semestre;
+    private double indiceReprobacion;
 
+    public HistorialAcademico() {}
+
+    public HistorialAcademico(double cargaAcademica, Carrera carrera, double indiceReprobacion, double procentajeBajas, double promedio, int semestre) {
+        this.cargaAcademica = cargaAcademica;
+        this.carrera = carrera;
+        this.indiceReprobacion = indiceReprobacion;
+        this.procentajeBajas = procentajeBajas;
+        this.promedio = promedio;
+        this.semestre = semestre;
+    }
+
+    public double getCargaAcademica() {
+        return cargaAcademica;
+    }
+
+    public void setCargaAcademica(double cargaAcademica) {
+        this.cargaAcademica = cargaAcademica;
+    }
+
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
+    }
+
+    public double getIndiceReprobacion() {
+        return indiceReprobacion;
+    }
+
+    public void setIndiceReprobacion(double indiceReprobacion) {
+        this.indiceReprobacion = indiceReprobacion;
+    }
+
+    public double getProcentajeBajas() {
+        return procentajeBajas;
+    }
+
+    public void setProcentajeBajas(double procentajeBajas) {
+        this.procentajeBajas = procentajeBajas;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
 }
