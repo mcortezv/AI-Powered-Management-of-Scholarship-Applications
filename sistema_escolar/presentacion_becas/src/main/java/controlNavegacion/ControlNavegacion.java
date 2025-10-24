fb385e6b9f44b02b1beddc885cd4375281e48511
janethcrgq <janethcrgq02@gmail.com>
2025-10-24 11:31:26 -0700

    control y pantallaInfo

---
+++ sistema_escolar/presentacion_becas/src/main/java/controlNavegacion/ControlNavegacion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlNavegacion;
+
+import presentacion.InfoGeneral;
+
+/**
+ *
+ * @author janethcristinagalvanquinonez
+ */
+public class ControlNavegacion {
+    
+    public void openFormInfoGeneral(){
+        new InfoGeneral().setVisible(true);
+    }
+}
