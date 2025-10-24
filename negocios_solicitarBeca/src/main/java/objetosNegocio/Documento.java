dffcf73a2138cabf17404fbc968d759b52516f76
mcortezv <mdjesuscv@gmail.com>
2025-10-23 18:22:30 -0700

    Tutor
    Co-authored-by: Janeth Galvan <janethcrgq02@gmail.com>

--- negocios_solicitarBeca/src/main/java/objetosNegocio/Documento.java
+++ negocios_solicitarBeca/src/main/java/objetosNegocio/Documento.java
@@ -4,11 +4,51 @@
  */
 
 package objetosNegocio;
+import objetosNegocio.enums.TipoDocumento;
 
 /**
  *
  * @author Cortez, Manuel;
  */
 public class Documento {
+    private int identificador;
+    private TipoDocumento tipo;
+    private Estudiante estudiante;
 
+    public Documento() {}
+
+    public Documento(TipoDocumento tipo, Estudiante estudiante) {
+        this.estudiante = estudiante;
+        this.tipo = tipo;
+    }
+
+    public Documento(int identificador, TipoDocumento tipo, Estudiante estudiante) {
+        this.estudiante = estudiante;
+        this.identificador = identificador;
+        this.tipo = tipo;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public TipoDocumento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoDocumento tipo) {
+        this.tipo = tipo;
+    }
 }
