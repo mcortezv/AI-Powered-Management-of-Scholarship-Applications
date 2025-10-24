e58538460fcc19ef8318b97ea508013237754849
SebastianEsc05 <sebastianescram01@gmail.com>
2025-10-24 07:22:00 -0700

    "Primera prueba de implementación de  Dtos"

---
+++ sistema_escolar/dto_negocios/src/main/java/dto/DocumentoDTO.java
@@ -0,0 +1,10 @@
+package dto;
+
+public class DocumentoDTO {
+    private long idEstudiante;
+    private String tipoDocumento;
+    private String archivoDocumentoBase64;
+    private String nombreDocumento;
+
+
+}
--- sistema_escolar/negocios_solicitarBeca/src/main/java/objetosNegocio/Requisitos.java
+++ sistema_escolar/negocios_solicitarBeca/src/main/java/objetosNegocio/Requisitos.java
@@ -5,10 +5,58 @@
 
 package objetosNegocio;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Cortez, Manuel;
  */
 public class Requisitos {
+    private double promedioMinimo;
+    private int semestreMinimo;
+    private double ingresoFamiliarMaximo;
+    private LocalDateTime fechaLimiteSolicitud;
 
+
+    public Requisitos() {
+    }
+
+    public Requisitos(double promedioMinimo, int semestreMinimo, double ingresoFamiliarMaximo, LocalDateTime fechaLimiteSolicitud) {
+        this.promedioMinimo = promedioMinimo;
+        this.semestreMinimo = semestreMinimo;
+        this.ingresoFamiliarMaximo = ingresoFamiliarMaximo;
+        this.fechaLimiteSolicitud = fechaLimiteSolicitud;
+    }
+
+    public double getPromedioMinimo() {
+        return promedioMinimo;
+    }
+
+    public void setPromedioMinimo(double promedioMinimo) {
+        this.promedioMinimo = promedioMinimo;
+    }
+
+    public int getSemestreMinimo() {
+        return semestreMinimo;
+    }
+
+    public void setSemestreMinimo(int semestreMinimo) {
+        this.semestreMinimo = semestreMinimo;
+    }
+
+    public double getIngresoFamiliarMaximo() {
+        return ingresoFamiliarMaximo;
+    }
+
+    public void setIngresoFamiliarMaximo(double ingresoFamiliarMaximo) {
+        this.ingresoFamiliarMaximo = ingresoFamiliarMaximo;
+    }
+
+    public LocalDateTime getFechaLimiteSolicitud() {
+        return fechaLimiteSolicitud;
+    }
+
+    public void setFechaLimiteSolicitud(LocalDateTime fechaLimiteSolicitud) {
+        this.fechaLimiteSolicitud = fechaLimiteSolicitud;
+    }
 }
