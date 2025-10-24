6d2dbff19240f5627c5e75b0a932aad570bf0239
SebastianEsc05 <sebastianescram01@gmail.com>
2025-10-24 07:22:00 -0700

    "Primera prueba de implementación de  Dtos"

--- dto_negocios/src/main/java/dto/SolicitudDTO.java
+++ dto_negocios/src/main/java/dto/SolicitudDTO.java
@@ -1,4 +1,12 @@
 package dto;
 
+import java.time.LocalDate;
+
 public class SolicitudDTO {
+    private long id;
+    private String estado;
+    private LocalDate fechaSolicitud;
+    private EstudianteDTO estudianteDTO;
+    private BecaDTO becaDTO;
+
 }
