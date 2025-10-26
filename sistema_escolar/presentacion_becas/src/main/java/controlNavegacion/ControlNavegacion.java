74c66ed001d0cde54bb0bc41479a3833acb89401
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-10-26 15:43:46 -0700

    paciencia

--- sistema_escolar/presentacion_becas/src/main/java/controlNavegacion/ControlNavegacion.java
+++ sistema_escolar/presentacion_becas/src/main/java/controlNavegacion/ControlNavegacion.java
@@ -4,6 +4,11 @@
  */
 package controlNavegacion;
 
+import presentacion.login.panels.IniciarSesionPanel;
+import presentacion.solicitarBeca.panels.ConfirmacionPanel;
+import presentacion.solicitarBeca.panels.HistorialAcademicoPanel;
+import presentacion.solicitarBeca.panels.InformacionGeneralPanel;
+
 
 
 /**
@@ -11,6 +16,65 @@ package controlNavegacion;
  * @author janethcristinagalvanquinonez
  */
 public class ControlNavegacion {
-    
-    
+//    private static ControlNavegacion instancia;
+//    
+//    private IniciarSesionPanel iniciarSesion;
+//    private InformacionGeneralPanel infoGeneral;
+//    private HistorialAcademicoPanel historialAcad;
+//    private ConfirmacionPanel confirmacionPanel;
+//    
+//    // aquí irá lo de los BOs 
+//    // aaa
+//    
+//    private ControlNavegacion(){
+//        // inicializarBOs();
+//        inicializarPantallas();
+//    }
+//    
+//    public static ControlNavegacion getInstancia() {
+//        if (instancia == null){
+//            instancia = new ControlNavegacion();
+//        }
+//        return instancia;
+//    }
+//    
+//    private void inicializarPantallas() {
+//        this.iniciarSesion = new IniciarSesionPanel();
+//        this.infoGeneral = new InformacionGeneralPanel();
+//        this.historialAcad = new HistorialAcademicoPanel();
+//        this.confirmacionPanel = new ConfirmacionPanel();
+//    }
+//    
+//    public void iniciar() {
+//        mostrarIniciarSesion();
+//    }
+//    
+//    public void mostrarIniciarSesion() {
+//        ocultarTodas();
+//        iniciarSesion.setVisible(true);
+//    }
+//
+//    public void mostrarInformacionGeneral() {
+//        ocultarTodas();
+//        infoGeneral.setVisible(true);
+//    }
+//
+//    public void mostrarHistorialAcademico() {
+//        ocultarTodas();
+//        historialAcad.setVisible(true);
+//    }
+//
+//    public void mostrarConfirmacion() {
+//        ocultarTodas();
+//        confirmacionPanel.setVisible(true);
+//    }
+//
+//    private void ocultarTodas() {
+//        iniciarSesion.setVisible(false);
+//        infoGeneral.setVisible(false);
+//        historialAcad.setVisible(false);
+//        confirmacionPanel.setVisible(false);
+//    }
+//    
+//    
 }
