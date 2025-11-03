bd94aa05cfcd492ca1b9dec54f35102c6b48bd45
SebastianEsc05 <sebastianescram01@gmail.com>
2025-11-02 17:33:02 -0700

    Modificacion de infraestructura gobierno

--- sistema_escolar/presentacion_becas/src/main/java/controlNavegacion/ControlNavegacion.java
+++ sistema_escolar/presentacion_becas/src/main/java/controlNavegacion/ControlNavegacion.java
@@ -127,17 +127,9 @@ public class ControlNavegacion {
         return solicitudDTO;
     }
 
-
-
-
-
-
     public EstudianteResponseDTO solicitarLogin(SolicitudLoginDTO solicitudLoginDTO){
         return manejador.validarInicioSesion(solicitudLoginDTO);
     }
 
-    
-   
-    
     
 }
