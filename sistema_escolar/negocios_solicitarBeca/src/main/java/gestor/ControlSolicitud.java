6825f33a02e8d49bc3d117c4bc7fa493fea954de
SebastianEsc05 <sebastianescram01@gmail.com>
2025-11-11 15:07:37 -0700

    rename de gestor y manejador en solicitarBeca

---
+++ sistema_escolar/negocios_solicitarBeca/src/main/java/gestor/ControlSolicitud.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestor;
+
+import dto.*;
+
+import objetosNegocio.mock.BecaBOMock;
+import objetosNegocio.mock.EstudianteMock;
+
+/**
+ *
+ * @author janethcristinagalvanquinonez
+ */
+public class ControlSolicitud {
+    
+    BecaBOMock becaBO;
+    private EstudianteMock estudianteMock = EstudianteMock.getInstance();
+
+    public SolicitudBecasDisponiblesResponseDTO obtenerBecasDisponibles(SolicitudBecasDisponiblesDTO solicitudDTO) {
+        return becaBO.getInstance().obtenerBecas();
+    }
+
+//
+//    public EstudianteResponseDTO validarInicioSesion(SolicitudLoginDTO solicitudLoginDTO) {
+//        String usuario = solicitudLoginDTO.getUsuario();
+//        String password = solicitudLoginDTO.getContrasenia();
+//
+//        Estudiante estudiante = null;
+//        if (usuario != null && usuario.matches("\\d+")) {
+//            estudiante = EstudianteMock.getInstance().buscarPorMatriculaYPassword(usuario, password);
+//        }
+//        if (estudiante == null) {
+//            estudiante = EstudianteMock.getInstance().buscarPorCorreoYPassword(usuario, password);
+//        }
+//        if (estudiante == null) return null;
+//
+//        return new EstudianteResponseDTO(
+//            "inscrito",
+//            9.8,
+//            estudiante.getCorreo(),
+//            estudiante.getDireccion() != null ? estudiante.getDireccion().getCalle() : "",
+//            estudiante.getTelefono(),
+//            "ISW",
+//            estudiante.getNombre(),
+//            estudiante.getMatricula() != null ? estudiante.getMatricula().intValue() : 0
+//        );
+//    }
+
+}
