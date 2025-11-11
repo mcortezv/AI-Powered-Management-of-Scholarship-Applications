5da4e20f4c9eb33a3b6810644cde9367fd7d9834
SebastianEsc05 <sebastianescram01@gmail.com>
2025-11-11 15:33:50 -0700

    Validar inicio de sesion

---
+++ presentacion_becas/src/main/java/presentacion/login/exceptions/CorreoInvalidoException.java
@@ -0,0 +1,7 @@
+package presentacion.login.exceptions;
+
+public class CorreoInvalidoException extends RuntimeException {
+    public CorreoInvalidoException(String message) {
+        super(message);
+    }
+}
