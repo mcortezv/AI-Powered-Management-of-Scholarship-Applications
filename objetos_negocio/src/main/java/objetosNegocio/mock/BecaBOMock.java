51720b1f932b9c4308a73638a89d8a007d0cf086
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-11-11 22:46:12 -0700

    cambios pantallas

--- objetos_negocio/src/main/java/objetosNegocio/mock/BecaBOMock.java
+++ objetos_negocio/src/main/java/objetosNegocio/mock/BecaBOMock.java
@@ -39,7 +39,8 @@ public class BecaBOMock {
         java.time.LocalDate ahora = java.time.LocalDate.now();
         List<BecaDTO> listaBecas = new ArrayList<>();
         
-        BecaDTO beca1 = new BecaDTO(1, "Apoyo Economico", "Moisés Vázquez Gudiño", "Dirigida a estudiantes con escasos recursos económicos ", "2025-2", 50, new RequisitosDTO(7.0, 12000.0, 30.0, 50.0, 30.0, false, true), ahora.plusMonths(1));               
+        BecaDTO beca1 = new BecaDTO(1, "Apoyo Economico", "Moisés Vázquez Gudiño", "Dirigida a estudiantes con escasos recursos económicos, Dirigida a estudiantes con escasos recursos económicos, Dirigida a estudiantes con escasos recursos económicos, Dirigida a estudiantes con escasos recursos económicos, "
+                + "Dirigida a estudiantes con escasos recursos económicos, Dirigida a estudiantes con escasos recursos económicos, Dirigida a estudiantes con escasos recursos económicos ", "2025-2", 50, new RequisitosDTO(7.0, 12000.0, 30.0, 50.0, 30.0, false, true), ahora.plusMonths(1));               
         BecaDTO beca2 = new BecaDTO(2, "Excelencia", "Beca Ara", "Dirigida a alumnos con excelencia academica y regulares con su plan de estudios", "2025-2", 20, new RequisitosDTO(9.0, 999999.0, 10.0, 70.0, 10.0, false, false), ahora.plusMonths(1));       
         BecaDTO beca3 = new BecaDTO(3, "Promedio", "Fundación Espinoza Rodríguez", "Está dirigida a estudiantes con buen promedio", "2025-2", 40, new RequisitosDTO(8.0, 15000.0, 20.0, 60.0, 20.0, false, false), ahora.plusMonths(1));
         BecaDTO beca4 = new BecaDTO(4, "Corporativa", "Constellation Brands", "Para impulsar las habilidades y capacidades de jóvenes universitarios.", "2025-2", 15, new RequisitosDTO(8.5, 20000.0, 15.0, 70.0, 15.0, true, false), ahora.plusMonths(1));
