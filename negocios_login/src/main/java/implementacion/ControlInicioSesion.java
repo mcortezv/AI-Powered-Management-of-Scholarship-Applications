874706eac96af76096f6ae6781c663ca61d144b3
SebastianEsc05 <sebastianescram01@gmail.com>
2025-11-15 13:48:33 -0700

    comentarios

--- negocios_login/src/main/java/implementacion/ControlInicioSesion.java
+++ negocios_login/src/main/java/implementacion/ControlInicioSesion.java
@@ -4,10 +4,14 @@ import dto.EstudianteResponseDTO;
 import dto.SolicitudLoginDTO;
 import objetosNegocio.mock.EstudianteMock;
 
+// todos los metodos para llevar a cabo las acciones
+
 public class ControlInicioSesion {
     private EstudianteMock estudianteMock= EstudianteMock.getInstance();
 
     public EstudianteResponseDTO solicitarLogin(SolicitudLoginDTO solicitudLoginDTO){
         return estudianteMock.getEstudianteResponseDTO(solicitudLoginDTO);
     }
+
+    // cerrrar sesion
 }
