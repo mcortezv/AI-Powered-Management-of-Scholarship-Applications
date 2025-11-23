0b27d63da1339b09ff7887777043fc172cabfec0
mcortezv <mdjesuscv@gmail.com>
2025-11-23 16:13:41 -0700

    Sistema Gobierno

---
+++ sistema_gobierno/objetos_negocio/src/main/java/objetosNegocio/Objetos_negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package objetosNegocio;
+
+/**
+ *
+ * @author mdjes
+ */
+public class Objetos_negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ sistema_gobierno/presentacion_gobierno/src/main/java/evaluarSolicitudes/Presentacion_gobierno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package evaluarSolicitudes;
+
+/**
+ *
+ * @author mdjes
+ */
+public class Presentacion_gobierno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
