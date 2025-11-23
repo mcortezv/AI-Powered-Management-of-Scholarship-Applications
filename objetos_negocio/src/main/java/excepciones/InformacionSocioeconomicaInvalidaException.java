80cfd74a8e7eaa8f09a388c20d9ad4fa69665ffa
mcortezv <mdjesuscv@gmail.com>
2025-11-23 01:21:24 -0700

    Excepciones

--- objetos_negocio/src/main/java/excepciones/InformacionSocioeconomicaInvalidaException.java
+++ objetos_negocio/src/main/java/excepciones/InformacionSocioeconomicaInvalidaException.java
@@ -1,5 +1,9 @@
 package excepciones;
 
+/**
+ *
+ * @author Cortez, Manuel;
+ */
 public class InformacionSocioeconomicaInvalidaException extends ValidacionException {
     public InformacionSocioeconomicaInvalidaException(String msg) { super(msg); }
 }
