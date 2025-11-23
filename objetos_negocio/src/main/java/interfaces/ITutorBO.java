e40ab703f54006f41e63c744656030bca6f2ae57
mcortezv <mdjesuscv@gmail.com>
2025-11-22 19:01:20 -0700

    Enviar Solicitud Gobierno Completa

--- objetos_negocio/src/main/java/interfaces/ITutorBO.java
+++ objetos_negocio/src/main/java/interfaces/ITutorBO.java
@@ -2,8 +2,11 @@ package interfaces;
 import dominio.*;
 import excepciones.*;
 
+/**
+ *
+ * @author Cortez, Manuel;
+ */
 public interface ITutorBO {
-    Tutor crearTutor(String nombre, String correo, String telefono,
-                     Direccion direccion)
-            throws TutorInvalidoException;
+
+    Tutor crearTutor(String nombre, String correo, String telefono, String direccion) throws TutorInvalidoException;
 }
