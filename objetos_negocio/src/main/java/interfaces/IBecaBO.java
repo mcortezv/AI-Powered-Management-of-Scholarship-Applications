8c6bf2cb5b363f55971b28b6ed08162377f7a952
mcortezv <mdjesuscv@gmail.com>
2025-11-23 15:28:28 -0700

    Stash

--- objetos_negocio/src/main/java/interfaces/IBecaBO.java
+++ objetos_negocio/src/main/java/interfaces/IBecaBO.java
@@ -2,6 +2,10 @@ package interfaces;
 import dominio.*;
 import excepciones.*;
 
+/**
+ *
+ * @author Cortez, Manuel;
+ */
 public interface IBecaBO {
     Beca obtenerBecaPorCodigo(int codigo) throws BecaInvalidaException;
 }
