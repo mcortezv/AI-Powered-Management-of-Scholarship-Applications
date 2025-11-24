26c19bba76a0c195c7535d9a59fb9c0cbc476eb8
mcortezv <mdjesuscv@gmail.com>
2025-11-23 20:11:47 -0700

    Estudiante Adaptador

--- sistema_gobierno/objetos_negocio/src/main/java/objetosNegocio/adaptadores/EstudianteAdaptador.java
+++ sistema_gobierno/objetos_negocio/src/main/java/objetosNegocio/adaptadores/EstudianteAdaptador.java
@@ -1,5 +1,7 @@
 package objetosNegocio.adaptadores;
 import datos.dominio.Estudiante;
+import datos.dominio.Tutor;
+import datos.dominio.enums.Carrera;
 import dto.EstudianteDTO;
 import dto.EstudianteInfraestructuraDTO;
 import objetosNegocio.adaptadores.excepciones.EstudianteAdaptadorException;
@@ -12,7 +14,15 @@ public class EstudianteAdaptador {
 
     public static Estudiante toEntity(EstudianteDTO dto){
         try {
-
+            Estudiante estudiante = new Estudiante();
+            estudiante.setMatricula(dto.getMatricula());
+            estudiante.setNombre(dto.getNombre());
+            estudiante.setCarrera(Carrera.valueOf(dto.getCarrera()));
+            estudiante.setTutor(TutorAdaptador.toEntity(dto.getTutor()));
+            estudiante.setTelefono(dto.getTelefono());
+            estudiante.setDireccion(dto.getDireccion());
+            estudiante.setCorreo(dto.getCorreo());
+            return estudiante;
         } catch (Exception sinUso){
             throw new EstudianteAdaptadorException("No se pudo mappear la DTO a Entidad");
         }
@@ -20,7 +30,15 @@ public class EstudianteAdaptador {
 
     public static Estudiante toEntity(EstudianteInfraestructuraDTO dto){
         try {
-
+            Estudiante estudiante = new Estudiante();
+            estudiante.setMatricula(dto.getMatricula());
+            estudiante.setNombre(dto.getNombre());
+            estudiante.setCarrera(Carrera.valueOf(dto.getCarrera()));
+            estudiante.setTutor(TutorAdaptador.toEntity(dto.getTutor()));
+            estudiante.setTelefono(dto.getTelefono());
+            estudiante.setDireccion(dto.getDireccion());
+            estudiante.setCorreo(dto.getCorreo());
+            return estudiante;
         } catch (Exception sinUso){
             throw new EstudianteAdaptadorException("No se pudo mappear la DTO de Infraestructura a Entidad");
         }
@@ -28,7 +46,15 @@ public class EstudianteAdaptador {
 
     public static EstudianteDTO toDTO(Estudiante estudiante){
         try {
-
+            EstudianteDTO dto = new EstudianteDTO();
+            dto.setMatricula(estudiante.getMatricula());
+            dto.setNombre(estudiante.getNombre());
+            dto.setCarrera(estudiante.getCarrera().toString());
+            dto.setTutor(TutorAdaptador.toDTO(estudiante.getTutor()));
+            dto.setTelefono(estudiante.getTelefono());
+            dto.setDireccion(estudiante.getDireccion());
+            dto.setCorreo(estudiante.getCorreo());
+            return dto;
         } catch (Exception sinUso){
             throw new EstudianteAdaptadorException("No se pudo mappear la Entidad a DTO");
         }
@@ -36,7 +62,15 @@ public class EstudianteAdaptador {
 
     public static EstudianteInfraestructuraDTO toInfraestructuraDTO(Estudiante  estudiante){
         try {
-
+            EstudianteInfraestructuraDTO dto = new EstudianteInfraestructuraDTO();
+            dto.setMatricula(estudiante.getMatricula());
+            dto.setNombre(estudiante.getNombre());
+            dto.setCarrera(estudiante.getCarrera().toString());
+            dto.setTutor(TutorAdaptador.toInfraestructuraDTO(estudiante.getTutor()));
+            dto.setTelefono(estudiante.getTelefono());
+            dto.setDireccion(estudiante.getDireccion());
+            dto.setCorreo(estudiante.getCorreo());
+            return dto;
         } catch (Exception sinUso){
             throw new EstudianteAdaptadorException("No se pudo mappear la Entidad a DTO Infraestructura");
         }
