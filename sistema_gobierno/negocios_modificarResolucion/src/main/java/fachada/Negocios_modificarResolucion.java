4f9b842e94cd3b3a37fec6564dddac27acd0ba28
mcortezv <mdjesuscv@gmail.com>
2025-11-23 17:32:46 -0700

    Sistema Gobierno

---
+++ sistema_gobierno/negocios_modificarResolucion/src/main/java/fachada/Negocios_modificarResolucion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fachada;
+
+/**
+ *
+ * @author mdjes
+ */
+public class Negocios_modificarResolucion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
