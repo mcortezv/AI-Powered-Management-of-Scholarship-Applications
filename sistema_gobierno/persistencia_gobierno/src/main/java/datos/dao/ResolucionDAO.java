cbb8496a65fa95dac95654443d8ece6683a1b52b
mcortezv <mdjesuscv@gmail.com>
2025-11-23 19:28:42 -0700

    DAO

--- sistema_gobierno/persistencia_gobierno/src/main/java/datos/dao/ResolucionDAO.java
+++ sistema_gobierno/persistencia_gobierno/src/main/java/datos/dao/ResolucionDAO.java
@@ -1,4 +1,36 @@
 package datos.dao;
+import datos.dao.interfaces.IResolucionDAO;
+import datos.dominio.Resolucion;
+import java.util.List;
 
-public class ResolucionDAO {
+/**
+ *
+ * @author Cortez, Manuel;
+ */
+public class ResolucionDAO implements IResolucionDAO {
+
+    @Override
+    public boolean guardar(Resolucion resolucion){
+        return true;
+    }
+
+    @Override
+    public boolean actualizar(Resolucion resolucion){
+        return true;
+    }
+
+    @Override
+    public Resolucion obtenerPorId(int id){
+        return null;
+    }
+
+    @Override
+    public Resolucion obtenerPorFiltro(String tipoFiltro, String filtro){
+        return null;
+    }
+
+    @Override
+    public List<Resolucion> obtenerTodas(){
+        return null;
+    }
 }
--- sistema_gobierno/persistencia_gobierno/src/main/java/datos/dao/interfaces/ISolicitudDAO.java
+++ sistema_gobierno/persistencia_gobierno/src/main/java/datos/dao/interfaces/ISolicitudDAO.java
@@ -1,4 +1,18 @@
 package datos.dao.interfaces;
+import datos.dominio.Solicitud;
+import java.util.List;
 
+/**
+ *
+ * @author Cortez, Manuel;
+ */
 public interface ISolicitudDAO {
+
+    List<Solicitud> obtenerPorConvocatoria(int idConvocatoria);
+
+    Solicitud obtenerPorId(int idSolicitud);
+
+    Solicitud obtenerPorFiltro(String tipoFiltro, String filtro);
+
+    boolean actualizar(Solicitud solicitud);
 }
