8d1bd501435e050b98e9adb76bf9a89ed0f23163
mcortezv <mdjesuscv@gmail.com>
2025-11-23 20:03:20 -0700

    Beca Adaptador

--- sistema_gobierno/objetos_negocio/src/main/java/objetosNegocio/adaptadores/BecaAdaptador.java
+++ sistema_gobierno/objetos_negocio/src/main/java/objetosNegocio/adaptadores/BecaAdaptador.java
@@ -1,5 +1,6 @@
 package objetosNegocio.adaptadores;
 import datos.dominio.Beca;
+import datos.dominio.enums.TipoBeca;
 import dto.BecaDTO;
 import dto.BecaInfraestructuraDTO;
 import objetosNegocio.adaptadores.excepciones.BecaAdaptadorException;
@@ -12,7 +13,17 @@ public class BecaAdaptador {
 
     public static Beca toEntity(BecaDTO dto){
         try {
-
+            Beca beca = new Beca();
+            beca.setCodigo(dto.getCodigo());
+            beca.setTipo(TipoBeca.valueOf(dto.getTipo()));
+            beca.setNombre(dto.getNombre());
+            beca.setDescripcion(dto.getDescripcion());
+            beca.setFechaInicio(dto.getFechaInicio());
+            beca.setFechaFin(dto.getFechaFin());
+            beca.setBecasDisponibles(dto.getBecasDisponibles());
+            beca.setRequisitos(RequisitosAdaptador.toEntity(dto.getRequisitos()));
+            beca.setFechaResultados(dto.getFechaResultados());
+            return beca;
         } catch (Exception sinUso){
             throw new BecaAdaptadorException("No se pudo mappear la DTO a Entidad");
         }
@@ -20,7 +31,17 @@ public class BecaAdaptador {
 
     public static Beca toEntity(BecaInfraestructuraDTO dto){
         try {
-
+            Beca beca = new Beca();
+            beca.setCodigo(dto.getCodigo());
+            beca.setTipo(TipoBeca.valueOf(dto.getTipo()));
+            beca.setNombre(dto.getNombre());
+            beca.setDescripcion(dto.getDescripcion());
+            beca.setFechaInicio(dto.getFechaInicio());
+            beca.setFechaFin(dto.getFechaFin());
+            beca.setBecasDisponibles(dto.getBecasDisponibles());
+            beca.setRequisitos(RequisitosAdaptador.toEntity(dto.getRequisitos()));
+            beca.setFechaResultados(dto.getFechaResultados());
+            return beca;
         } catch (Exception sinUso){
             throw new BecaAdaptadorException("No se pudo mappear la DTO de Infraestructura a Entidad");
         }
@@ -28,7 +49,17 @@ public class BecaAdaptador {
 
     public static BecaDTO toDTO(Beca beca){
         try {
-
+            BecaDTO dto = new BecaDTO();
+            dto.setCodigo(beca.getCodigo());
+            dto.setTipo(beca.getTipo().toString());
+            dto.setNombre(beca.getNombre());
+            dto.setDescripcion(beca.getDescripcion());
+            dto.setFechaInicio(beca.getFechaInicio());
+            dto.setFechaFin(beca.getFechaFin());
+            dto.setBecasDisponibles(beca.getBecasDisponibles());
+            dto.setRequisitos(RequisitosAdaptador.toDTO(beca.getRequisitos()));
+            dto.setFechaResultados(beca.getFechaResultados());
+            return dto;
         } catch (Exception sinUso){
             throw new BecaAdaptadorException("No se pudo mappear la Entidad a DTO");
         }
@@ -36,7 +67,17 @@ public class BecaAdaptador {
 
     public static BecaInfraestructuraDTO toInfraestructuraDTO(Beca  beca){
         try {
-
+            BecaInfraestructuraDTO dto = new BecaInfraestructuraDTO();
+            dto.setCodigo(beca.getCodigo());
+            dto.setTipo(beca.getTipo().toString());
+            dto.setNombre(beca.getNombre());
+            dto.setDescripcion(beca.getDescripcion());
+            dto.setFechaInicio(beca.getFechaInicio());
+            dto.setFechaFin(beca.getFechaFin());
+            dto.setBecasDisponibles(beca.getBecasDisponibles());
+            dto.setRequisitos(RequisitosAdaptador.toInfraestructuraDTO(beca.getRequisitos()));
+            dto.setFechaResultados(beca.getFechaResultados());
+            return dto;
         } catch (Exception sinUso){
             throw new BecaAdaptadorException("No se pudo mappear la Entidad a DTO Infraestructura");
         }
---
+++ sistema_gobierno/objetos_negocio/src/main/java/objetosNegocio/bo/excepciones/ResolucionBOException.java
@@ -0,0 +1,11 @@
+package objetosNegocio.bo.excepciones;
+
+/**
+ *
+ * @author Cortez, Manuel;
+ */
+public class ResolucionBOException extends RuntimeException {
+    public ResolucionBOException(String message) {
+        super(message);
+    }
+}
