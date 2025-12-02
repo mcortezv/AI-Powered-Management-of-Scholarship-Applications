10b8dcf49c02d6b0036e2c9d3d20dfaf15628841
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-12-02 16:16:02 -0700

    paquetes persistencia tutorias

---
+++ sistema_escolar/persistencia_tutorias/src/main/java/tutorias/dao/a.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorias.dao;
+
+/**
+ *
+ * @author katia
+ */
+public class a {
+    
+}
