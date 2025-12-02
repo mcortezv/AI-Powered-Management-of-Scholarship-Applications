418928afd862ec7dd94b6e26426765863d5d74c9
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-12-02 15:41:18 -0700

    persistencia tutorias

---
+++ sistema_escolar/persistencia_tutorias/src/main/java/mcortezv/persistencia_tutorias/Persistencia_tutorias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mcortezv.persistencia_tutorias;
+
+/**
+ *
+ * @author katia
+ */
+public class Persistencia_tutorias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
